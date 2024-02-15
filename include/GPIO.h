2c92af58f2f9aa2626bf8fabc1009cd58c5df8c2
Omar Tarek Omar <tarekomar37@gmail.com>
2024-02-15 09:56:31 +0200

    minor update

--- include/GPIO.h
+++ include/GPIO.h
@@ -71,5 +71,5 @@ typedef enum
 
 GPIO_ERROR_STATE GPIO_InitPin(GPIO_CONFIG_T * Config);
 GPIO_ERROR_STATE GPIO_SetPinValue(uint32_t GPIO_PORT, uint32_t GPIO_PIN, uint32_t PIN_STATE);
-GPIO_ERROR_STATE GPIO_GetPinValue(uint32_t GPIO_PORT, uint32_t GPIO_PIN, uint8_t * PinValue);           //FIXME:Implement
+GPIO_ERROR_STATE GPIO_GetPinValue(uint32_t GPIO_PORT, uint32_t GPIO_PIN, uint8_t * PinValue);          
 
